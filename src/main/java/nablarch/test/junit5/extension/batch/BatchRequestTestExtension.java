53ea103a41c9b97a5ef1e5ce874f01e7de2d5494
opengl-8080 <opengl-8080@users.noreply.github.com>
2022-01-18 16:07:28 +0900

    feat: BatchRequestTest実装

--- src/main/java/nablarch/test/junit5/extension/batch/BatchRequestTestExtension.java
+++ src/main/java/nablarch/test/junit5/extension/batch/BatchRequestTestExtension.java
@@ -1,4 +1,16 @@
 package nablarch.test.junit5.extension.batch;
 
-public class BatchRequestTestExtension {
+import nablarch.test.core.batch.BatchRequestTestSupport;
+import nablarch.test.junit5.extension.event.TestEventDispatcherExtension;
+import org.junit.jupiter.api.extension.ExtensionContext;
+
+/**
+ * {@link BatchRequestTestSupport} を JUnit 5 で使用するための Extension 実装。
+ * @author Tanaka Tomoyuki
+ */
+public class BatchRequestTestExtension extends TestEventDispatcherExtension<BatchRequestTestSupport> {
+    @Override
+    protected BatchRequestTestSupport createSupport(Object testInstance, ExtensionContext context) {
+        return new BatchRequestTestSupport(testInstance.getClass());
+    }
 }
---
+++ src/test/java/nablarch/test/junit5/extension/batch/BatchRequestTestExtensionTest.java
@@ -0,0 +1,24 @@
+package nablarch.test.junit5.extension.batch;
+
+import nablarch.test.core.batch.BatchRequestTestSupport;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+/**
+ * {@link BatchRequestTestExtension} の単体テスト。
+ *
+ * @author Tanaka Tomoyuki
+ */
+@BatchRequestTest
+public class BatchRequestTestExtensionTest {
+
+    public BatchRequestTestSupport support;
+
+    @Test
+    public void BatchRequestTestExtensionが動作していることをテスト() {
+        assertThat(support, is(instanceOf(BatchRequestTestSupport.class)));
+    }
+}
