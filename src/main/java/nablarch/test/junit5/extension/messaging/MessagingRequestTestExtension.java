20be31d16accb4f8b4cd8e060101b6ed0620172d
opengl-8080 <opengl-8080@users.noreply.github.com>
2022-01-19 16:21:57 +0900

    feat: MessagingRequestTestExtension実装

--- src/main/java/nablarch/test/junit5/extension/messaging/MessagingRequestTestExtension.java
+++ src/main/java/nablarch/test/junit5/extension/messaging/MessagingRequestTestExtension.java
@@ -1,4 +1,16 @@
 package nablarch.test.junit5.extension.messaging;
 
-public class MessagingRequestTestExtension {
+import nablarch.test.core.messaging.MessagingRequestTestSupport;
+import nablarch.test.junit5.extension.event.TestEventDispatcherExtension;
+import org.junit.jupiter.api.extension.ExtensionContext;
+
+/**
+ * {@link MessagingRequestTestSupport} を JUnit 5 で使用するための Extension 実装。
+ * @author Tanaka Tomoyuki
+ */
+public class MessagingRequestTestExtension extends TestEventDispatcherExtension<MessagingRequestTestSupport> {
+    @Override
+    protected MessagingRequestTestSupport createSupport(Object testInstance, ExtensionContext context) {
+        return new MessagingRequestTestSupport(testInstance.getClass());
+    }
 }
