6b509380026ba1b321b1867d18ff97db55368b7b
opengl-8080 <opengl-8080@users.noreply.github.com>
2022-01-19 16:24:23 +0900

    feat: HttpRequestTestExtension実装

--- src/main/java/nablarch/test/junit5/extension/http/HttpRequestTestExtension.java
+++ src/main/java/nablarch/test/junit5/extension/http/HttpRequestTestExtension.java
@@ -1,4 +1,16 @@
 package nablarch.test.junit5.extension.http;
 
-public class HttpRequestTestExtension {
+import nablarch.test.core.http.HttpRequestTestSupport;
+import nablarch.test.junit5.extension.event.TestEventDispatcherExtension;
+import org.junit.jupiter.api.extension.ExtensionContext;
+
+/**
+ * {@link HttpRequestTestSupport} を JUnit 5 で使用するための Extension 実装。
+ * @author Tanaka Tomoyuki
+ */
+public class HttpRequestTestExtension extends TestEventDispatcherExtension<HttpRequestTestSupport> {
+    @Override
+    protected HttpRequestTestSupport createSupport(Object testInstance, ExtensionContext context) {
+        return new HttpRequestTestSupport(testInstance.getClass());
+    }
 }
