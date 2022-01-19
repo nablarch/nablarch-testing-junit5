a1736de8045fcebf174893dc235b18ba8a41323e
opengl-8080 <opengl-8080@users.noreply.github.com>
2022-01-19 18:59:59 +0900

    feat: RestTestExtension 実装

--- src/main/java/nablarch/test/junit5/extension/http/RestTestExtension.java
+++ src/main/java/nablarch/test/junit5/extension/http/RestTestExtension.java
@@ -1,4 +1,26 @@
 package nablarch.test.junit5.extension.http;
 
-public class RestTestExtension {
+import nablarch.test.core.http.RestTestSupport;
+import nablarch.test.core.http.SimpleRestTestSupport;
+import nablarch.test.junit5.extension.event.TestEventDispatcherExtension;
+import org.junit.jupiter.api.extension.ExtensionContext;
+
+/**
+ * {@link RestTestSupport} を JUnit 5 で使用するための Extension 実装。
+ * @author Tanaka Tomoyuki
+ */
+public class RestTestExtension extends SimpleRestTestExtension {
+    private RestTestSupport support;
+
+    @Override
+    protected RestTestSupport createSupport(Object testInstance, ExtensionContext context) {
+        support = new RestTestSupport();
+        return support;
+    }
+
+    @Override
+    public void beforeEach(ExtensionContext context) {
+        super.beforeEach(context);
+        support.setUpDb();
+    }
 }
