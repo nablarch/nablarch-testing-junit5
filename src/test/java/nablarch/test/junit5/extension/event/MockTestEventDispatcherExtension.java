932710605a37e4c4d677e98a05bb07261361576a
opengl-8080 <opengl-8080@users.noreply.github.com>
2022-01-19 16:07:42 +0900

    refactor: MockTestEventDispatcherExtensionの型パラメータを設定

--- src/test/java/nablarch/test/junit5/extension/event/MockTestEventDispatcherExtension.java
+++ src/test/java/nablarch/test/junit5/extension/event/MockTestEventDispatcherExtension.java
@@ -1,16 +1,15 @@
 package nablarch.test.junit5.extension.event;
 
-import nablarch.test.event.TestEventDispatcher;
 import org.junit.jupiter.api.extension.ExtensionContext;
 
 /**
  * テストのための {@link TestEventDispatcherExtension} の仮実装クラス。
  * @author Tanaka Tomoyuki
  */
-public class MockTestEventDispatcherExtension extends TestEventDispatcherExtension {
+public class MockTestEventDispatcherExtension extends TestEventDispatcherExtension<MockTestEventDispatcher> {
 
     @Override
-    protected TestEventDispatcher createSupport(Object testInstance, ExtensionContext context) {
+    protected MockTestEventDispatcher createSupport(Object testInstance, ExtensionContext context) {
         return new MockTestEventDispatcher();
     }
 }
