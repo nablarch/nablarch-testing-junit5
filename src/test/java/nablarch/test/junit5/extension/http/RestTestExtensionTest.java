807e38223a6e3338196cbb8069b5926bbcc60cce
opengl-8080 <opengl-8080@users.noreply.github.com>
2023-04-27 20:12:03 +0900

    test: jmockit -> mockito (#8)

--- src/test/java/nablarch/test/junit5/extension/http/RestTestExtensionTest.java
+++ src/test/java/nablarch/test/junit5/extension/http/RestTestExtensionTest.java
@@ -1,56 +1,58 @@
 package nablarch.test.junit5.extension.http;
 
-import mockit.Expectations;
-import mockit.Mocked;
-import mockit.Verifications;
 import nablarch.test.core.http.RestTestSupport;
 import nablarch.test.junit5.extension.MockExtensionContext;
+import nablarch.test.support.reflection.ReflectionUtil;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtensionContext;
-
-import java.util.List;
+import org.mockito.MockedConstruction;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.sameInstance;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.Mockito.mockConstruction;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
 
 /**
  * {@link RestTestExtension}の単体テスト。
  * @author Tanaka Tomoyuki
  */
 public class RestTestExtensionTest {
-    RestTestSupport support;
-
     final RestTestExtension sut = new RestTestExtension();
 
     @Test
     void beforeEachを実行すると_TestRuleが再現され_RestTestSupport_SimpleRestTestSupport_TestEventDispatcherのテスト前処理が実行されることをテスト() throws Exception {
         sut.postProcessTestInstance(this, null);
 
-        new Expectations(support) {{
-            support.setUpDb(); times = 1;
-            support.setUp(); times = 1;
-            support.dispatchEventOfBeforeTestMethod(); times = 1;
-        }};
+        final RestTestSupport originalSupport = ReflectionUtil.getFieldValue(sut, "support");
+        final RestTestSupport spiedSupport = spy(originalSupport);
+        ReflectionUtil.setFieldValue(sut, "support", spiedSupport);
 
         ExtensionContext context = new MockExtensionContext(RestTestExtensionTest.class,
                 RestTestExtensionTest.class.getDeclaredMethod("testForMock"));
 
         sut.beforeEach(context);
 
-        assertThat(support.testName.getMethodName(), is("testForMock"));
-        assertThat(support.testDescription.getTestClass(), is(equalTo(RestTestExtensionTest.class)));
+        verify(spiedSupport).setUpDb();
+        verify(spiedSupport).setUp();
+        verify(spiedSupport).dispatchEventOfBeforeTestMethod();
+
+        assertThat(spiedSupport.testName.getMethodName(), is("testForMock"));
+        assertThat(spiedSupport.testDescription.getTestClass(), is(equalTo(RestTestExtensionTest.class)));
     }
 
     @Test
-    void RestTestSupportのインスタンス生成時にテストクラスが渡されていることをテスト(@Mocked RestTestSupport mockSupport) throws Exception {
-        RestTestSupport created = sut.createSupport(this, null);
+    void RestTestSupportのインスタンス生成時にテストクラスが渡されていることをテスト() throws Exception {
+        final RestTestSupportMockInitializer initializer = new RestTestSupportMockInitializer();
 
-        new Verifications() {{
-            List<RestTestSupport> captured = withCapture(new RestTestSupport(RestTestExtensionTest.class)); times = 1;
-            assertThat(captured.get(0), is(sameInstance(created)));
-        }};
+        try (final MockedConstruction<RestTestSupport> mocked = mockConstruction(RestTestSupport.class, initializer)) {
+            RestTestSupport created = sut.createSupport(this, null);
+            
+            assertThat(initializer.testType, is(sameInstance(RestTestExtensionTest.class)));
+            assertThat(initializer.mock, is(sameInstance(created)));
+        }
     }
 
     /**
@@ -59,5 +61,16 @@ public class RestTestExtensionTest {
      */
     private void testForMock() {
         // noop
+    }
+    
+    private static class RestTestSupportMockInitializer implements MockedConstruction.MockInitializer<RestTestSupport> {
+        private RestTestSupport mock;
+        private Class<?> testType;
+
+        @Override
+        public void prepare(RestTestSupport mock, MockedConstruction.Context context) {
+            this.mock = mock;
+            testType = (Class<?>) context.arguments().get(0);
+        }
     }
 }
--- src/test/java/nablarch/test/junit5/extension/integration/IntegrationTestExtensionIntegrationTest.java
+++ src/test/java/nablarch/test/junit5/extension/integration/IntegrationTestExtensionIntegrationTest.java
@@ -1,14 +1,16 @@
 package nablarch.test.junit5.extension.integration;
 
-import mockit.Mocked;
-import mockit.Verifications;
 import nablarch.test.core.db.DbAccessTestSupport;
 import nablarch.test.core.integration.IntegrationTestSupport;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedConstruction;
 
 import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.Mockito.mockConstruction;
+import static org.mockito.Mockito.verify;
 
 /**
  * {@link IntegrationTestExtension} を実際に JUnit 5 で動かすテスト。
@@ -17,17 +19,20 @@ import static org.hamcrest.MatcherAssert.assertThat;
  */
 @IntegrationTest
 class IntegrationTestExtensionIntegrationTest {
-    @Mocked
-    DbAccessTestSupport mockDbAccessTestSupport;
+    final MockedConstruction<DbAccessTestSupport> mocked = mockConstruction(DbAccessTestSupport.class);
 
     IntegrationTestSupport support;
+
+    @AfterEach
+    void tearDown() {
+        mocked.close();
+    }
 
     @Test
     void IntegrationTestExtensionが動作することをテスト() {
         assertThat(support, is(instanceOf(IntegrationTestSupport.class)));
 
-        new Verifications() {{
-            mockDbAccessTestSupport.setUpDb("setUpDb"); times = 1;
-        }};
+        final DbAccessTestSupport mockDbAccessTestSupport = mocked.constructed().get(0);
+        verify(mockDbAccessTestSupport).setUpDb("setUpDb");
     }
 }
