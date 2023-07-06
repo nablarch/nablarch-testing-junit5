268f80cd1bd8da3433a0c55e893f18fbe360eb0c
hijiri <3445928+hiji@users.noreply.github.com>
2023-07-06 17:37:10 +0900

    デバッグ

--- src/test/java/nablarch/test/junit5/extension/db/DbAccessTestExtensionIntegrationTest.java
+++ src/test/java/nablarch/test/junit5/extension/db/DbAccessTestExtensionIntegrationTest.java
@@ -1,7 +1,12 @@
 package nablarch.test.junit5.extension.db;
 
+import nablarch.core.db.connection.ConnectionFactory;
+import nablarch.core.db.transaction.SimpleDbTransactionManager;
 import nablarch.test.core.db.DbAccessTestSupport;
 import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Field;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.CoreMatchers.is;
@@ -17,7 +22,17 @@ public class DbAccessTestExtensionIntegrationTest {
     DbAccessTestSupport support;
 
     @Test
-    void DbAccessTestExtensionが適用できていることをテスト() {
+    void DbAccessTestExtensionが適用できていることをテスト() throws Exception {
+        Field f1 = DbAccessTestSupport.class.getDeclaredField("transactionManagers");
+        f1.setAccessible(true);
+        List<SimpleDbTransactionManager> trans = (List<SimpleDbTransactionManager>) f1.get(support);
+        SimpleDbTransactionManager defaultTransactionManager = trans.get(0);
+        Field f2 = SimpleDbTransactionManager.class.getDeclaredField("connectionFactory");
+        f2.setAccessible(true);
+        ConnectionFactory connectionFactory = (ConnectionFactory) f2.get(defaultTransactionManager);
+
+        System.out.println("####################### " + connectionFactory.getClass().getSimpleName() + " ######################");
+
         assertThat(support, is(instanceOf(DbAccessTestSupport.class)));
     }
 }
