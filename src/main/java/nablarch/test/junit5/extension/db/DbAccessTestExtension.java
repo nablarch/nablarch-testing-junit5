1ae29bac534e9f38c64b18db8e04d1eea07e3e43
hijiri <3445928+hiji@users.noreply.github.com>
2023-07-06 16:18:49 +0900

    デバッグ

--- src/main/java/nablarch/test/junit5/extension/db/DbAccessTestExtension.java
+++ src/main/java/nablarch/test/junit5/extension/db/DbAccessTestExtension.java
@@ -5,7 +5,7 @@ import nablarch.test.core.db.DbAccessTestSupport;
 import nablarch.test.junit5.extension.event.TestEventDispatcherExtension;
 import org.junit.jupiter.api.extension.ExtensionContext;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 /**
@@ -16,20 +16,20 @@ import java.time.format.DateTimeFormatter;
 public class DbAccessTestExtension extends TestEventDispatcherExtension {
     @Override
     protected DbAccessTestSupport createSupport(Object testInstance, ExtensionContext context) {
-        System.out.println(now() + " !!!!!!!!!!!!!!!!!!!!!! " + Thread.currentThread().getId() + " createSupport start...");
+        System.out.println(now() + " " + context.getDisplayName() + " !!!!!!!!!!!!!!!!!!!!!! " + Thread.currentThread().getId() + " createSupport start...");
         try {
             return new DbAccessTestSupport(testInstance.getClass());
         } catch (Exception e) {
             e.printStackTrace();
             throw e;
         } finally {
-            System.out.println(now() + " !!!!!!!!!!!!!!!!!!!!!! " + Thread.currentThread().getId() + " createSupport end!!");
+            System.out.println(now() + " " + context.getDisplayName() + " !!!!!!!!!!!!!!!!!!!!!! " + Thread.currentThread().getId() + " createSupport end!!");
         }
     }
 
     @Override
     public void beforeEach(ExtensionContext context) throws Exception {
-        System.out.println(now() + " !!!!!!!!!!!!!!!!!!!!!! " + Thread.currentThread().getId() + " beforeEach start...");
+        System.out.println(now() + " " + context.getDisplayName() + " !!!!!!!!!!!!!!!!!!!!!! " + Thread.currentThread().getId() + " beforeEach start...");
         try {
             super.beforeEach(context);
             ((DbAccessTestSupport) support).beginTransactions();
@@ -37,13 +37,13 @@ public class DbAccessTestExtension extends TestEventDispatcherExtension {
             e.printStackTrace();
             throw e;
         } finally {
-            System.out.println(now() + " !!!!!!!!!!!!!!!!!!!!!! " + Thread.currentThread().getId() + " beforeEach end!!");
+            System.out.println(now() + " " + context.getDisplayName() + " !!!!!!!!!!!!!!!!!!!!!! " + Thread.currentThread().getId() + " beforeEach end!!");
         }
     }
 
     @Override
     public void afterEach(ExtensionContext context) throws Exception {
-        System.out.println(now() + " !!!!!!!!!!!!!!!!!!!!!! " + Thread.currentThread().getId() + " afterEach start...");
+        System.out.println(now() + " " + context.getDisplayName() + " !!!!!!!!!!!!!!!!!!!!!! " + Thread.currentThread().getId() + " afterEach start...");
         try {
             super.afterEach(context);
             ((DbAccessTestSupport) support).endTransactions();
@@ -51,11 +51,11 @@ public class DbAccessTestExtension extends TestEventDispatcherExtension {
             e.printStackTrace();
             throw e;
         } finally {
-            System.out.println(now() + " !!!!!!!!!!!!!!!!!!!!!! " + Thread.currentThread().getId() + " afterEach end!!");
+            System.out.println(now() + " " + context.getDisplayName() + " !!!!!!!!!!!!!!!!!!!!!! " + Thread.currentThread().getId() + " afterEach end!!");
         }
     }
 
     private String now() {
-        return LocalDate.now().toString();
+        return LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME);
     }
 }
